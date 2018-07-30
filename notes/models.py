25bee82dd22b6b83515c3c5e87ff52baac36acdc
jaivonmassena <jaivonmassena@gmail.com>
2018-07-30 22:45:23 +0200

    working on real app now

---
+++ notes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    last_modified = models.DateTimeField(auto_now = True)
