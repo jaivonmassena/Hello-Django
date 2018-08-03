388dffcf39edf23cd8485ce97ad356d8f946e8c2
jaivonmassena <jaivonmassena@gmail.com>
2018-08-03 11:57:44 +0200

    starting from scratch day one complete

--- notes/admin.py
+++ notes/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from .models import Note
-# Register your models here.
 
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content','created_at', 'last_modified', 'author')
-    list_filter = ('title', 'created_at', 'author', 'last_modified')
-    
-admin.site.register(Note, NoteAdmin)
+# Register your models here.
