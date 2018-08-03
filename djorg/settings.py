8ef2fc49dd626c2ad767d1cd13f5e6574b1cd134
jaivonmassena <jaivonmassena@gmail.com>
2018-08-03 16:41:29 +0200

    day 2,3 & 4 are done

--- djorg/settings.py
+++ djorg/settings.py
@@ -38,7 +38,20 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notes',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
